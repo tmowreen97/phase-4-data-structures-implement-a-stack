0605042e24aa3f2edc43979e0c18e85f6b39d0e9
Tasreen Mowreen <tmowreen97@gmail.com>
2023-02-26 17:38:38 -0500

    commit tree 02cd4e7be49bd650d00be9ea79123297c7ad86fd

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,54 @@
 # your code here
+require 'pry'
+class Stack
+  def initialize(limit=nil)
+    @stack = []
+    @limit = limit
+  end
+
+  def push value
+    @stack.push(value)
+    if @stack.length == @limit
+      return 'Stack Overflow'
+    end
+  end
+
+  def pop 
+    @stack.pop
+  end
+
+  def peek
+    @stack.last
+  end
+
+  def size
+    @stack.length
+  end
+
+  def empty?
+    @stack.empty?
+  end
+
+  def full?
+    @stack.length == @limit ? true : false
+  end
+
+  def search value
+    counter = @stack.length-1
+    @stack.each do |val|
+      if val == value
+        return counter
+      else 
+        counter-=1
+      end
+    end
+    counter
+  end
+
+  def StackOverflowError
+    'Stack Overflow'
+  end
+end
+
+
+
